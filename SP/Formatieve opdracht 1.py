30a0ee2aa02e0eb91962ebe6488385cd93053319
AHipWhale <charlie.choffat@gmail.com>
2020-02-04 14:03:01 +0100

    Opdracht 1

--- SP/Formatieve opdracht 1.py
+++ SP/Formatieve opdracht 1.py
@@ -1,4 +1,5 @@
 input=input('Hoe groot? ')
+
 for i in range(int(input)):
     print('*'*(i+1))
 x=int(input)
@@ -13,3 +14,14 @@ while i!=int(input):
 while i!=0:
     i-=1
     print('*'*i)
+
+space=int(input)
+for i in range(int(input)):
+    space-=1
+    print(' '*space+'*'*(i+1))
+x=int(input)
+space=0
+for i in range(int(input)):
+    space+=1
+    x-=1
+    print(' '*space+'*'*x)
