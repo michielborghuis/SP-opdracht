6632c8b6cf69c425e7662e2d47d60da4fa36ea75
AHipWhale <charlie.choffat@gmail.com>
2020-02-05 14:49:52 +0100

    Opdracht 3

--- SP/Formatieve opdracht 3.py
+++ SP/Formatieve opdracht 3.py
@@ -1,15 +1,14 @@
 #a.
-lijst = [1, 5, 5, 5, 3, 6, 7, 9, 0]
-def count(lijst):
-    getal=int(input('Van welk getal wil jij weten hoe vaak het voorkomt in de lijst? '))
+lijst1 = [1, 5, 5, 5, 3, 6, 7, 9, 0]
+def count(lijst,getal):
     hoeveel=0
     for i in lijst:
         if getal == i:
             hoeveel+=1
-    print('Het getal '+str(getal)+' komt '+str(hoeveel)+' keer voor in de lijst')
+    return hoeveel
 
 #b.
-lijst=[5,5,7,3,8,5,8,9,4,8,1,2,6,0]
+lijst2=[5,5,7,3,8,5,8,9,4,8,1,2,6,0]
 def verschil(lijst):
     x=0
     for i in lijst:
@@ -17,4 +16,18 @@ def verschil(lijst):
         if z>x:
             x=z
     print(x)
-verschil(lijst)
+verschil(lijst2)
+
+#c.
+lijst3=[0,1,1,1,0,0,0,1,1,1,0]
+def goed(lijst):
+    x=int(count(lijst,1))
+    y=int(count(lijst,0))
+    z=int(count(lijst,0))
+    if x < y:
+        return print('De lijst voldoet niet aan de eis: Het aantal enen is groter dan aan het aantal nullen')
+    elif z > 12:
+        return print('De lijst voldoet niet aan de eis: Er mogen niet meer dan 12 nullen zijn')
+    else:
+        return print('De lijst voldoet aan alle eisen')
+goed(lijst3)
