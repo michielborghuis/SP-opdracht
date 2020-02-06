c5c5c6c011a0b292a4951287a22eb9f17368021d
AHipWhale <charlie.choffat@gmail.com>
2020-02-06 19:33:18 +0100

    Opdracht 11

---
+++ SP/Formatieve opdracht 11.py
@@ -0,0 +1,24 @@
+alfa='abcdefghijklmnopqrstuvwxyz'
+tekst=input('Geef een tekst: ')
+cijfer=int(input('Geef een rotatie: '))
+code=''
+while cijfer > 26:
+    cijfer -= 26
+
+for i in tekst:
+    lower=0
+    if i==' ' or i=='.' or i=='!' or i=='?':
+        code=code+i
+    else:
+        if i not in alfa:
+            i=i.lower()
+            lower=1
+        x=alfa.index(i)
+        z=x+cijfer
+        y=alfa[z]
+        if lower==1:
+            code=code+y.upper()
+        else:
+            code=code+y
+        lower=0
+print('Ceadercode: '+str(code))
