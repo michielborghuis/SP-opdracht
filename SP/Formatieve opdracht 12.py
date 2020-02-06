cd8eb8357fcc72ec8d20e6a16fab990b336c15b6
AHipWhale <charlie.choffat@gmail.com>
2020-02-06 23:01:04 +0100

    Opdracht 12

--- SP/Formatieve opdracht 12.py
+++ SP/Formatieve opdracht 12.py
@@ -2,8 +2,8 @@ for i in range(1,101):
     if i%3==0 and i%5!=0:
         print('Fizz')
     elif i%5==0 and i%3!=0:
-        print('Bizz')
+        print('Buzz')
     elif i%5==0 and i%3==0:
-        print('FizzBizz')
+        print('FizzBuzz')
     else:
         print(i)
